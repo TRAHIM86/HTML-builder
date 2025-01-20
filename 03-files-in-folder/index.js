cb744133f19f603895251db70448be5e30db5241
TRAHIM86 <trahim1986@gmail.com>
2025-01-20 17:29:51 +0300

    fix: changed adding filename, if the name contains a dot

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,15 +8,17 @@ function getFileInfo() {
   readingFiles
     .then(
       (files) => {        
-        files.forEach((file) => {                       //для каждого file в files
-          if (file.isFile()) {                          //проверим на исфайл()
-            let fileName = file.name                    //вытянем имя и формат ('text.txt')
+        files.forEach((file) => {                         //для каждого file в files
+          if (file.isFile()) {                            //проверим на исфайл()
+            let lastIndex = file.name.lastIndexOf('.');   //индекс последней точки (если имя файла имя.еще.разш)
 
-            let fileExt = path.extname(fileName);       //вытянем формат (txt')
+            let fileName = file.name.slice(0, lastIndex);
+
+            let fileExt = path.extname(file.name);         //вытянем формат (txt')
                     
-            fs.stat(path.join(pathToFolder, fileName), (err, stats) =>{    //стата для файла     
+            fs.stat(path.join(pathToFolder, file.name), (err, stats) =>{    //стата для файла     
               if (err) {console.log(`Sorry, error: ${err}.`)}              //если еррор - покажем еррор
-              else {console.log(`${fileName.split('.')[0]} - ${fileExt.split('.')[1]} - ${stats.size}b`)}  //результат в консоль по каждому файлу
+              else {console.log(`${fileName} - ${fileExt.split('.')[1]} - ${stats.size}b`)}  //результат в консоль по каждому файлу
             })
           }
         })
